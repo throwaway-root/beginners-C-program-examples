46560d12f7a6adb68084111600362df3772611d3
naveenbalachandran <bnaveen360@gmail.com>
2020-10-08 12:00:57 +0530

    Added Comments
    Added comments to addition.c

--- Addition.c
+++ Addition.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 void main()
-{ int a,b,c;
+{ int a,b,c;//Declare Variables
 printf("Enter two Numbers : ");
-scanf("%d %d",&a,&b);
+scanf("%d %d",&a,&b);//Take input from keyboard
 c=a+b;
-printf("The Sum is %d",c);
+printf("The Sum is %d",c);//Show Results
 }
