ee9325d0d09a2c5b9ffc9a5f70ad097722e8e120
nathanielyeldo <56597570+nathanielyeldo@users.noreply.github.com>
2020-10-08 12:25:54 +0530

    Sentences edited.c

--- Area_of_Triangle.c
+++ Area_of_Triangle.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void main()
 { float a,b,h;
-printf("Enter the base : ");
+printf("Enter the base of the triangle : ");
 scanf("%f",&b);
-printf("Enter the height : ");
+printf("Enter the height of the triangle : ");
 scanf("%f",&h);
 a=0.5*b*h;
-printf("Area is %f",a);
+printf("Area is the triangle %f",a);
 }
